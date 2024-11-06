5293004f40f12c3366a05f0c6990bf21d0c7c6f6
Yash Mathur <114582885+yashmathur865@users.noreply.github.com>
2024-11-06 21:02:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates from a Sorted Linked List/remove-duplicates-from-a-sorted-linked-list.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+/* package whatever; // don't place package name! */
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) { this.data = data; }
+}
+
+class Remove_Duplicate_From_LL {
+    Node head;
+    Node tail;
+
+    public void addToTheLast(Node node) {
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+    }
+
+    void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    /* Drier program to test above functions */
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while (t > 0) {
+            String s[] = in.readLine().trim().split(" ");
+            Node head = new Node(Integer.parseInt(s[0]));
+            Node copy = head;
+            for (int i = 1; i < s.length; i++) {
+                Node temp = new Node(Integer.parseInt(s[i]));
+                copy.next = temp;
+                copy = copy.next;
+            }
+            Solution ob = new Solution();
+            Node ans = ob.removeDuplicates(head);
+            while (ans != null) {
+                System.out.print(ans.data + " ");
+                ans = ans.next;
+            }
+            System.out.println();
+            System.out.println("~");
+
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+*/
+
+class Solution {
+    // Function to remove duplicates from sorted linked list.
+    Node removeDuplicates(Node head) {
+        // Your code here
+        Node curr = head;
+        if(curr==null) return null;
+        while(curr.next!=null){
+            if(curr.data==curr.next.data){
+                curr.next=curr.next.next;
+            }
+            else{
+                curr=curr.next;
+            }
+        }
+        return head;
+    }
+}
