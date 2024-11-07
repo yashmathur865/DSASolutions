478a1f95647f73aa80dbbf135c846dc8ded4bd54
Yash Mathur <114582885+yashmathur865@users.noreply.github.com>
2024-11-07 10:43:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Reverse a Linked List in Groups/reverse-a-linked-list-in-groups.java
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int key) {
+        data = key;
+        next = null;
+    }
+}
+
+class ReverseInSize {
+    static Node head;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(in.readLine().trim());
+
+        while (t-- > 0) {
+
+            String s[] = in.readLine().trim().split(" ");
+            int a1 = Integer.parseInt(s[0]);
+            Node head = new Node(a1);
+            Node tail = head;
+            for (int i = 1; i < s.length; i++) {
+                int a = Integer.parseInt(s[i]);
+                // addToTheLast(new Node(a));
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+
+            int k = Integer.parseInt(in.readLine().trim());
+            Solution ob = new Solution();
+            Node res = ob.reverse(head, k);
+            printList(res, out);
+            out.println();
+
+            out.println("~");
+        }
+        out.close();
+    }
+
+    public static void printList(Node node, PrintWriter out) {
+        while (node != null) {
+            out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*node class of the linked list
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+*/
+
+class Solution {
+    public static Node reverse(Node head, int k) {
+        // Your code here
+        //7-11-24
+        Node curr=head,prevFirst=null;
+        boolean isFirstPass = true;
+        while(curr!=null){
+            Node first=curr,prev=null;
+            int count=0;
+            while(curr!=null && count<k){
+                Node next=curr.next;
+                curr.next=prev;
+                prev=curr;
+                curr=next;
+                count++;
+            }
+            if(isFirstPass){
+                head=prev;
+                isFirstPass=false;
+            }
+            else{
+                prevFirst.next=prev;
+            }
+            prevFirst=first;
+        }
+        return head;
+    }
+}
