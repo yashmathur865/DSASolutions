6a496bf2f571f5fb44bce06c947f7b8223c1e318
Yash Mathur <114582885+yashmathur865@users.noreply.github.com>
2024-11-13 10:41:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Greater Element/next-greater-element.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Geeks {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine()); // Number of test cases
+        for (int g = 0; g < t; g++) {
+            String[] str =
+                (br.readLine()).trim().split(" "); // Reading input as a string array
+            int arr[] = new int[str.length]; // Creating integer array from the input
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            // Getting the result from the Solution class
+            ArrayList<Integer> result = new Solution().nextLargerElement(arr);
+
+            // Printing the result in the required format
+            if (result.isEmpty()) {
+                System.out.println("[]");
+            } else {
+                for (int i = 0; i < result.size(); i++) {
+                    if (i != 0) System.out.print(" ");
+                    System.out.print(result.get(i));
+                }
+                System.out.println();
+                System.out.println("~");
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    //13-11-24
+    // Function to find the next greater element for each element of the array.
+    public ArrayList<Integer> nextLargerElement(int[] arr) {
+        // code here
+        int n = arr.length;
+        ArrayList<Integer> ans = new ArrayList<>();
+        ArrayDeque<Integer> s = new ArrayDeque<>();
+        
+        for(int i=n-1;i>=0;i--){
+            while(s.isEmpty()==false && arr[i]>=s.peek()){
+                s.pop();
+            }
+            int res = s.isEmpty()?-1:s.peek();
+            ans.add(0,res); // Adding at beginning instead of last because we are traversing in reverse
+            s.push(arr[i]);
+        }
+        return ans;
+    }
+}
